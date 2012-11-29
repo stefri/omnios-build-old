50ed96df2397c579840d2bdd6666a57635038bf3
Steffen Fritzsche <steffen.fritzsche@uni-ulm.de>
2012-11-29 17:08:59 +0100

    Fixed pkgsvr path

--- lib/site.sh
+++ lib/site.sh
@@ -1,7 +1,6 @@
 # Package server URL and publisher
 PKGPUBLISHER=uulm.mawi
-#PKGSRVR=http://scott.mathematik.uni-ulm.de:10000/
-PKGSVR=file:///repository/publisher/uulm.mawi/
+PKGSRVR=http://scott.mathematik.uni-ulm.de:10000/
 
 # Uncommenting this line will create a on-disk repo in
 # the top level of your checkout and publish there instead
