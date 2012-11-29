22bfdd1a95863d85ee1e95ea60d164af60ea87c1
Steffen Fritzsche <steffen.fritzsche@uni-ulm.de>
2012-11-29 16:24:05 +0100

    Changed publisher port

--- lib/site.sh
+++ lib/site.sh
@@ -1,6 +1,6 @@
 # Package server URL and publisher
 PKGPUBLISHER=uulm.mawi
-PKGSRVR=http://scott.mathematik.uni-ulm.de/
+PKGSRVR=http://scott.mathematik.uni-ulm.de:10000/
 
 # Uncommenting this line will create a on-disk repo in
 # the top level of your checkout and publish there instead
