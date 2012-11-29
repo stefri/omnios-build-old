9224629902e526f5cdaa48a056b19575ab5c2f59
Steffen Fritzsche <steffen.fritzsche@uni-ulm.de>
2012-11-29 16:58:38 +0100

    Testing local filesystem publisher

--- lib/site.sh
+++ lib/site.sh
@@ -1,6 +1,7 @@
 # Package server URL and publisher
 PKGPUBLISHER=uulm.mawi
-PKGSRVR=http://scott.mathematik.uni-ulm.de:10000/
+#PKGSRVR=http://scott.mathematik.uni-ulm.de:10000/
+PKGSVR=file:///repository/publisher/uulm.mawi/
 
 # Uncommenting this line will create a on-disk repo in
 # the top level of your checkout and publish there instead
