42551aaa665d65cb898998ba5a23eab445659c85
Steffen Fritzsche <steffen.fritzsche@uni-ulm.de>
2013-09-15 18:15:00 +0200

    Added tls support

--- build/postfix/build.sh
+++ build/postfix/build.sh
@@ -48,16 +48,13 @@ make_clean() {
 
 CONFIGURE_OPTS_32=""
 CONFIGURE_OPTS_64=""
-CONFIGURE_OPTS='-DNO_NIS -DHAS_PGSQL -DUSE_SASL_AUTH -DUSE_CYRUS_SASL'
+CONFIGURE_OPTS='-DNO_NIS -DHAS_PGSQL -DHAS_DB -DHAS_LDAP -DUSE_SASL_AUTH -DUSE_CYRUS_SASL -DUSE_TLS'
 CONFIGURE_CMD=create_makefiles
 
 create_makefiles() {
-    CCARGS='-DDEF_COMMAND_DIR=\"/usr/local/sbin\" -DDEF_DAEMON_DIR=\"/usr/local/libexec/postfix\" -DHAS_PGSQL -DHAS_DB -DHAS_LDAP -DUSE_SASL_AUTH -DUSE_CYRUS_SASL -I/usr/local/include -I/usr/local/include/sasl'
-    AUXLIBS="-R/usr/local/lib -L/usr/local/lib -ldb \
-             -R/usr/local/lib -L/usr/local/lib -lsasl2 \
-             -R/usr/local/lib -L/usr/local/lib -lldap \
-             -R/usr/local/lib -L/usr/local/lib -llber \
-             -R/usr/local/lib -L/usr/local/lib -lpq"
+    CCARGS='-DDEF_COMMAND_DIR=\"/usr/local/sbin\" -DDEF_DAEMON_DIR=\"/usr/local/libexec/postfix\" -DHAS_PGSQL -DHAS_DB -DHAS_LDAP -DUSE_SASL_AUTH -DUSE_CYRUS_SASL -DUSE_TLS -I/usr/local/include -I/usr/local/include/sasl -I/usr/include'
+    AUXLIBS="-R/usr/local/lib -L/usr/local/lib -ldb -lsasl2 -lldap -llber -lpq\
+             -R/usr/lib -L/usr/lib -lssl -lcrypto"
     logmsg "--- creating postfix makefiles"
     $MAKE -f Makefile.init makefiles CCARGS="$CCARGS $CONFIGURE_OPTS" AUXLIBS="$AUXLIBS"
     unset CCARGS
