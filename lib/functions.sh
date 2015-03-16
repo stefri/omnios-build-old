3fc2a5f7c4182d90cb8da4416e0412100bdf5f1a
Steffen Kram <steffen.kram@uni-ulm.de>
2015-03-16 14:20:13 +0100

    Added releases 151010 and 151012

--- lib/functions.sh
+++ lib/functions.sh
@@ -237,6 +237,14 @@ case "$RELVER" in
         GCCPKG="developer/gcc48"
         GCCPATH="/opt/gcc-4.8.1/bin"
         ;;
+    151010)
+        GCCPKG="developer/gcc48"
+        GCCPATH="/opt/gcc-4.8.1/bin"
+        ;;
+    151012)
+        GCCPKG="developer/gcc48"
+        GCCPATH="/opt/gcc-4.8.1/bin"
+        ;;
     *)
         echo "update lib/functions.sh this for unrecognized release $RELVER"
         exit 2
